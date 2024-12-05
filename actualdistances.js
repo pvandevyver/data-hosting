eb640672101c68a25fcd5727e36976af26dae0ed
pvandevyver <156080998+pvandevyver@users.noreply.github.com>
2024-12-06 00:13:58 +0100

    Delete actualdistances.js

--- actualdistances.js
+++
@@ -1,8 +0,0 @@
-actualDistances = [
-{"key":"4671 - DE Juechen EDC-DPD Mönchengladbach","value":135608},
-{"key":"DPD Innsbruck-DPD Innsbruck","value":0},
-{"key":"DPD Mönchengladbach-DPD Innsbruck","value":727670},
-{"key":"DPD Linz-DPD Linz","value":0},
-{"key":"DPD Mönchengladbach-DPD Linz","value":796624},
-{"key":"DPD Linz-DPD Graz","value":219918}
-];
