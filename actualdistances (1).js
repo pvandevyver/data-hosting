d87248e609d267d2dc7181f184b479ebe79fb3fb
pvandevyver <156080998+pvandevyver@users.noreply.github.com>
2024-12-05 23:24:45 +0100

    Add files via upload

---
+++ actualdistances (1).js
@@ -0,0 +1,14 @@
+actualDistances = {
+"0": [object Object],
+"1": [object Object],
+"2": [object Object],
+"3": [object Object],
+"4": [object Object],
+"5": [object Object],
+"DPD Innsbruck-DPD Innsbruck": 0,
+"4671 - DE Juechen EDC-DPD Mönchengladbach": 135608,
+"DPD Linz-DPD Linz": 0,
+"DPD Linz-DPD Graz": 219918,
+"DPD Mönchengladbach-DPD Innsbruck": 727670,
+"DPD Mönchengladbach-DPD Linz": 797716
+};
