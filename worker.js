89b5272612f91428de9d56c87685b1e62a3b2078
pvandevyver <156080998+pvandevyver@users.noreply.github.com>
2025-01-09 12:12:22 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,9 +1,3 @@
 self.onmessage = (event) => {
-  const { taskId, data } = event.data;
-
-  // Simulate a time-consuming task (e.g., multiplying input by 2)
-  const result = data.input * 2;
-  setTimeout(() => {
-    self.postMessage({ taskId, result });
-  }, 2000); // Simulate a delay
+  self.postMessage(`Received: ${event.data}`);
 };
