4c54002bfc49de778785e7e2a295104e21700648
pvandevyver <156080998+pvandevyver@users.noreply.github.com>
2025-03-15 11:47:59 +0100

    Update actualdistances.js

--- actualdistances.js
+++ actualdistances.js
@@ -401,6 +401,7 @@ window.actualDistances = {
 "DHL Freight Mechelen|DHL Freight Oudsbergen": 112062,
 "DHL Freight Mechelen|DHL Freight Herentals": 48645,
 "4671 - DE Juechen EDC|DHL GF DUS  Duesseldorf": 270688,
-
+"Airport Barcelona|Barce": 8001,
+"Ghent|Airport Antwerp": 80406
   
 };
